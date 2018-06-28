5b82685de1d8718296cb41882079a2e7f457d102
arugupta <arungupta26@yahoo.com>
2018-06-28 10:45:19 +0530

    binary gap solution

---
+++ src/com/test/codility/lessons/binarygap/Solution.java
@@ -0,0 +1,35 @@
+package com.test.codility.lessons.binarygap;
+
+public class Solution {
+
+    public int solution(int N) {
+
+        int maxGap = 0;
+        boolean firstOne = true;
+        int gap = 0;
+        while (N != 0) {
+            int bit = N % 2;
+            if (bit == 0) {
+                gap++;
+            } else {
+                if (firstOne) {
+                    firstOne = false;
+                } else {
+                    maxGap = Math.max(maxGap, gap);
+                }
+                gap = 0;
+            }
+            N /= 2;
+        }
+        return maxGap;
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+        Solution s = new Solution();
+        System.out.println(s.solution(1041));
+    }
+}
